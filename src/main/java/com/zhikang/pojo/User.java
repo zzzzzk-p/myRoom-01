f0bdd8d7fc687c5b7a1108aa238b12d24f95064a
zzzzzk <1169830536@qq.com>
2020-11-19 17:02:51 +0800

    初始化

---
+++ src/main/java/com/zhikang/pojo/User.java
@@ -0,0 +1,12 @@
+package com.zhikang.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @Author：panzhikang
+ * @Date: 2020/11/19 16:50
+ */
+public class User implements Serializable {
+    private String username;
+    private String password;
+}
